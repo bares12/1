3d8bec54926f675aa3a4e9a3a64ccf310d8e5be5
bares12 <40828949+bares12@users.noreply.github.com>
2018-07-09 05:24:41 +0000

    Update config.py

--- LineAPI/linepy/config.py
+++ LineAPI/linepy/config.py
@@ -28,7 +28,7 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[304]
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
     APP_VER     = '1.4.17'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'HelloWorld'
